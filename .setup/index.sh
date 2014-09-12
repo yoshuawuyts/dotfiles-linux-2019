b145f85eb5841047677c701a09982b3fc6aebf9c
yoshuawuyts <i@yoshuawuyts.com>
2014-09-12 09:33:00 +0200

    setup: move files around

--- .setup/index.sh
+++ .setup/index.sh
@@ -2,14 +2,6 @@
 
 ################################################################################
 #
-# Load OS X configuration.
-#
-################################################################################
-
-source ~/.setup/osx.sh
-
-################################################################################
-#
 # Provision OS X.
 #
 ################################################################################
@@ -17,6 +9,21 @@ source ~/.setup/osx.sh
 brew bundle ~/.setup/Brewfile
 brew bundle ~/.setup/Caskfile
 
+brew bundle ~/.setup/Brewfile
+brew bundle ~/.setup/Caskfile
+
+brew bundle ~/.setup/Brewfile
+brew bundle ~/.setup/Caskfile
+
 curl -sSL https://get.rvm.io | bash -s stable
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+
+################################################################################
+#
+# Load OS X configuration.
+#
+################################################################################
+
+source ~/.setup/osx.sh
+git config --global credential.helper osxkeychain
