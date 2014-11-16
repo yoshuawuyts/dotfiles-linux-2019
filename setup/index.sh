0c1a9551a769ca35d1fc1e779d71ef34c103a14d
yoshuawuyts <i@yoshuawuyts.com>
2014-11-16 21:45:55 +0100

    setup: update

--- setup/index.sh
+++ setup/index.sh
@@ -13,14 +13,14 @@ install_caskDeps() {
 }
 
 install_languages() {
-  curl -sSL https://get.rvm.io | bash -s stable
-  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
-  bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+  curl -sSL https://get.rvm.io | bash -s stable;
+  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)";
+  bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer);
 }
 
 configure_osx() {
-  source ~/.setup/osx.sh
-  git config --global credential.helper osxkeychain
+  source ~/.setup/osx.sh;
+  git config --global credential.helper osxkeychain;
 }
 
 main() {
