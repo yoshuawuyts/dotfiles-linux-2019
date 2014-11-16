2b2f0264c810ec94080e8d0dcd37cc23d578e2f9
yoshuawuyts <i@yoshuawuyts.com>
2014-11-16 16:11:56 +0100

    setup/index: clean

--- setup/index.sh
+++ setup/index.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 
-################################################################################
-#
-# Provision OS X.
-#
-################################################################################
+install_brewDeps() {
+  brew bundle ~/.setup/Brewfile;
+  brew bundle ~/.setup/Brewfile;
+  brew bundle ~/.setup/Brewfile;
+}
 
-brew bundle ~/.setup/Brewfile
-brew bundle ~/.setup/Caskfile
+install_caskDeps() {
+  brew bundle ~/.setup/Caskfile;
+  brew bundle ~/.setup/Caskfile;
+  brew bundle ~/.setup/Caskfile;
+}
 
-brew bundle ~/.setup/Brewfile
-brew bundle ~/.setup/Caskfile
+install_languages() {
+  curl -sSL https://get.rvm.io | bash -s stable
+  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+  bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+}
 
-brew bundle ~/.setup/Brewfile
-brew bundle ~/.setup/Caskfile
+configure_osx() {
+  source ~/.setup/osx.sh
+  git config --global credential.helper osxkeychain
+}
 
-curl -sSL https://get.rvm.io | bash -s stable
-ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
-bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+main() {
+  install_brewDeps;
+  install_caskDeps;
+  install_languages;
+  configure_osx;
+}
 
-################################################################################
-#
-# Load OS X configuration.
-#
-################################################################################
-
-source ~/.setup/osx.sh
-git config --global credential.helper osxkeychain
+main;
