a332b2e1cf0f5c1155484b11bbbb9ba698f54ac1
yoshuawuyts <i@yoshuawuyts.com>
2014-11-18 16:03:32 +0100

    osx: fix keyboard repeat stuffs

--- setup/osx.sh
+++ setup/osx.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 configure_keyboard() {
-  # Disable press-and-hold for keys in favor of key repeat
-  defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
-
   # Set a blazingly fast keyboard repeat rate
   defaults write NSGlobalDomain KeyRepeat -int 0
 }
