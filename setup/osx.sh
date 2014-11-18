194b2f3dd859ec406cceef9e3a629d9be5f555cf
yoshuawuyts <i@yoshuawuyts.com>
2014-11-18 15:48:37 +0100

    osx: update

--- setup/osx.sh
+++ setup/osx.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-main() {
+configure_keyboard() {
+  # Disable press-and-hold for keys in favor of key repeat
+  defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
+  # Set a blazingly fast keyboard repeat rate
+  defaults write NSGlobalDomain KeyRepeat -int 0
+}
+
+configure_screenshot_location() {
   defaults write com.apple.screencapture location ~/Google\ Drive/Screenshots;
   killall SystemUIServer;
+}
+
+main() {
+  configure_keyboard;
+  configure_screenshot_location;
 }
 
 main;
