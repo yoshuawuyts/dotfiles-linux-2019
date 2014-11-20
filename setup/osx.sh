5e40e78a83ebd719429b2e1313e25e565b834115
yoshuawuyts <i@yoshuawuyts.com>
2014-11-20 16:23:52 +0100

    setup/osx: update

--- setup/osx.sh
+++ setup/osx.sh
@@ -8,11 +8,6 @@ configure_keyboard() {
   defaults write NSGlobalDomain KeyRepeat -int 0
 }
 
-configure_screenshot_location() {
-  defaults write com.apple.screencapture location ~/Google\ Drive/Screenshots;
-  killall SystemUIServer;
-}
-
 configure_trackpad() {
   defaults write com.apple.AppleMultitouchTrackpad TrackpadRotate -bool true;
   defaults write com.apple.AppleMultitouchTrackpad TrackpadRightClick -bool true;
@@ -20,10 +15,20 @@ configure_trackpad() {
   defaults write com.apple.AppleMultitouchTrackpad TrackpadFourFingerPinchGesture 2;
 }
 
+configure_screenshot_location() {
+  defaults write com.apple.screencapture location ~/Google\ Drive/Screenshots;
+  killall SystemUIServer;
+}
+
+configure_dock() {
+  defaults write com.apple.screencapture location ~/Google\ Drive/Screenshots;
+}
+
 main() {
   configure_trackpad;
   configure_keyboard;
   configure_screenshot_location;
+  configure_dock;
 }
 
 main;
