d97de2e15a2ed02659802e7a47cf574ea62c1df6
yoshuawuyts <i@yoshuawuyts.com>
2015-01-20 09:41:16 +0100

    bin: add

--- .exports.sh
+++ .exports.sh
@@ -1,36 +1,6 @@
-#
-# User configuration
-#
-
-configure_path() {
-  export PATH="$PATH:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
-}
-
-configure_docker() {
-  export DOCKER_HOST=tcp://$(boot2docker ip 2>/dev/null):2375;
-}
-
-
-configure_terraform() {
-  export PATH=$PATH:~/Library/Terraform;
-}
-
-configure_node() {
-  export NVM_DIR=~/.nvm;
-}
-
-configure_go() {
-  if [ "" = "${GOPATH}" ]; then
-    # export GOPATH="/some/dir"
-  fi
-}
-
-main() {
-  configure_path;
-  configure_docker;
-  configure_terraform;
-  configure_node;
-  configure_go;
-}
-
-main;
+export PATH="$PATH:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+export DOCKER_HOST=tcp://$(boot2docker ip 2>/dev/null):2375
+export PATH=$PATH:~/Library/Terraform
+export PATH=$PATH:~/src/yw/binsauce
+export NVM_DIR=~/.nvm
+export EDITOR=/usr/bin/vim
--- .zshrc
+++ .zshrc
@@ -1,22 +1,14 @@
-#
+
 # Oh-my-zsh.
-#
+ZSH_THEME="pure"
+plugins=(git)
+export ZSH=$HOME/.oh-my-zsh
+source $ZSH/oh-my-zsh.sh
 
-ZSH_THEME="pure";
-plugins=(git);
-export ZSH=$HOME/.oh-my-zsh;
-source $ZSH/oh-my-zsh.sh;
-
-#
 # Imports.
-#
+source ~/.exports.sh
+source ~/.aliases.sh
 
-source ~/.exports.sh;
-source ~/.aliases.sh;
-
-#
 # Version managers.
-#
-
-source $(brew --prefix nvm)/nvm.sh;
-source ~/.rvm/scripts/rvm;
+source $(brew --prefix nvm)/nvm.sh
+source ~/.rvm/scripts/rvm
