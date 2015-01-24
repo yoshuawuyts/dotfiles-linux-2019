52f2f73b648c037128bb64b0afcb448236e76371
Yoshua Wuyts <i@yoshuawuyts.com>
2015-01-24 22:57:09 +0100

    zsh: update

---
+++ zsh/aliases.zsh
@@ -0,0 +1,70 @@
+alias cls='clear'
+alias l='ls -la'
+alias lastfm='shell-fm'
+alias reload!='. ~/.zshrc'
+alias count='wc -l'
+alias tft='curl "http://itsthisforthat.com/api.php?text"'
+
+alias shout="tr '[[:lower:]]' '[[:upper:]]'"
+
+# IP addresses
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+alias localip="ipconfig getifaddr en1"
+alias ips="ifconfig -a | grep -o 'inet6\? \(\([0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\)\|[a-fA-F0-9:]\+\)' | sed -e 's/inet6* //'"
+
+# Enhanced WHOIS lookups
+alias whois="whois -h whois-servers.net"
+
+# Flush Directory Service cache
+alias flush="dscacheutil -flushcache"
+
+# View HTTP traffic
+alias sniff="sudo ngrep -d 'en1' -t '^(GET|POST) ' 'tcp and port 80'"
+alias httpdump="sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
+
+# Recursively delete `.DS_Store` files
+alias ds_nomore="find . -type f -name '*.DS_Store' -ls -delete"
+
+# ROT13-encode text. Works for decoding, too! ;)
+alias rot13='tr a-zA-Z n-za-mN-ZA-M'
+
+# Show/hide hidden files in Finder
+alias show="defaults write com.apple.Finder AppleShowAllFiles -bool true && killall Finder"
+alias hide="defaults write com.apple.Finder AppleShowAllFiles -bool false && killall Finder"
+
+# Hide/show all desktop icons (useful when presenting)
+alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
+alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
+
+# color aliases
+alias grcurl="colourify curl -s"
+
+# zmv ftw
+autoload -U zmv
+# alias for zmv for no quotes
+# mmv *.c.orig orig/*.c
+alias mmv='noglob zmv -W'
+
+# from @nvie https://coderwall.com/p/4tkkpq
+#
+# ls **/*.zsh | map dirname
+alias map="xargs -n1"
+
+# free up `w` for my flavored sub
+alias whodat="w"
+
+# list all
+alias la="gls -ohpgn --color=auto --group-directories-first -o"
+
+# git status
+alias s="git status"
+
+# Recursively show all files in a folder
+# as a tree
+alias t='tree -a -L 3 -A -C -I "node_modules|.git"'
+
+# Launch vlc
+alias vlc='~/Applications/VLC.app/Contents/MacOS/VLC'
+
+# Improve `tmux` command
+alias tmux="TERM=screen-256color-bce tmux -u2"
---
+++ zsh/common.zsh
@@ -0,0 +1,20 @@
+export GHI_PAGER=less
+export EDITOR="vim"
+
+function colours() {
+for i in {0..255} ; do
+  printf "\x1b[38;5;${i}mcolour${i}\n"
+done
+}
+
+function trash() { mv $1 ~/.Trash }
+
+# find shorthand
+function f() {
+  find . -name "$1"
+}
+
+# cd into whatever is the forefront Finder window.
+cdf() {  # short for cdfinder
+  cd "`osascript -e 'tell app "Finder" to POSIX path of (insertion location as alias)'`"
+}
