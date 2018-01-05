6f522960c32e322b9ef921a85b54dec0c9c61301
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-01-05 13:56:41 +0100

    enable tlp

--- systemd/install.sh
+++ systemd/install.sh
@@ -12,3 +12,9 @@ _sudo_enable "xsecurelock@$(whoami).service"
 # Allow retrieving lat,long
 sudo systemctl enable gpsd
 sudo systemctl start gpsd
+
+# Enable power management
+sudo systemctl enable tlp.service
+sudo systemctl start tlp.service
+sudo systemctl enable tlp-sleep.service
+sudo systemctl start tlp-sleep.service
