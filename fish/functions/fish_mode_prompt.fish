3be5a34ad03e0ed0d74580100983451a7244b6ed
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-01-23 02:06:11 +0800

    add fish insert modes

--- fish/functions/fish_mode_prompt.fish
+++ fish/functions/fish_mode_prompt.fish
@@ -1,3 +1,13 @@
 function fish_mode_prompt
-  # NOOP - Disable vim mode indicator
+  switch $fish_bind_mode
+    case default
+      set_color green
+    case insert
+      set_color blue
+    case visual
+      set_color magenta
+  end
+
+  printf '\n[%s]' (basename $PWD)
+  set_color normal
 end
--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,7 +1,4 @@
 function fish_prompt
-  set_color blue
-  printf '\n[%s]' (basename $PWD)
-  set_color normal
   printf ' %s' (git-branch-status)
 
   set_color $fish_color_cwd
