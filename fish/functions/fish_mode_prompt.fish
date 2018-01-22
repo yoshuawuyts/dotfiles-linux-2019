f714f744b079b4adde2f7a611b014121d0c4278e
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-01-23 02:25:05 +0800

    revert fish insert modes

--- fish/functions/fish_mode_prompt.fish
+++ fish/functions/fish_mode_prompt.fish
@@ -1,13 +1,13 @@
-function fish_mode_prompt
-  switch $fish_bind_mode
-    case default
-      set_color green
-    case insert
-      set_color blue
-    case visual
-      set_color magenta
-  end
+function fish_mode_prompt --description "Displays the current mode"
+  # switch $fish_bind_mode
+  #   case default
+  #     set_color green
+  #   case insert
+  #     set_color blue
+  #   case visual
+  #     set_color magenta
+  # end
 
-  printf '\n[%s]' (basename $PWD)
-  set_color normal
+  # printf '\n[%s]' (basename $PWD)
+  # set_color normal
 end
--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,4 +1,7 @@
 function fish_prompt
+  set_color blue
+  printf '\n[%s]' (basename $PWD)
+  set_color normal
   printf ' %s' (git-branch-status)
 
   set_color $fish_color_cwd
