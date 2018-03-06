1628e7945e6af248c8ba5a965d1744876810c43b
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-03-06 22:21:17 +0100

    t

--- fish/config.fish
+++ fish/config.fish
@@ -7,4 +7,3 @@ alias gh='hub browse'
 alias ls='ls --color=auto --group-directories-first'
 alias ll='ls -alph --color=auto --group-directories-first'
 alias s='git status --short'
-alias t="tree -L 3"
