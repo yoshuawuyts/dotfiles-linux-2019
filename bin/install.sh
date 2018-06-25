9b93d09b76cffe9a20f9e66a9c54b5660a71ce24
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-06-25 17:39:01 +0200

    err cmd

--- bin/install.sh
+++ bin/install.sh
@@ -4,6 +4,7 @@ dirname="$(dirname "$(readlink -f "$0")")"
 source "$dirname/../scripts/_shared.sh"
 
 for f in $dirname/*; do
+  chmod +x "$f"
   extname="$(echo "$f" | tr '.' ' ' | awk '{ print $2 }')"
   [ "$extname" = 'sh' ] && continue
   dst="/usr/local/bin$(echo "$f" | sed "s|$dirname||")"
