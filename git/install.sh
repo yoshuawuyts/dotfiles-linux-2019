2fd5dbf0db8c10ec0d882966fb228187e159df48
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-08-26 01:39:26 +0200

    add git-signed-off-by to all commits!
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- git/install.sh
+++ git/install.sh
@@ -6,3 +6,7 @@ source "$dirname/../scripts/_shared.sh"
 src="$dirname/gitconfig"
 dst="$HOME/.gitconfig"
 _link "$src" "$dst"
+
+src="$dirname/hooks"
+dst="$HOME/.config/git/hooks"
+_link "$src" "$dst"
