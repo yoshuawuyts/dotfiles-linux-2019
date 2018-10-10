bd7bc541d02ecbfebdada660daa7439ca15acb8f
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-10-10 00:03:19 -0700

    working c cmd
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- zsh/install.sh
+++ zsh/install.sh
@@ -3,16 +3,16 @@
 dirname="$(dirname "$(readlink -f "$0")")"
 source "$dirname/../scripts/_shared.sh"
 
-mkdir -p "$HOME/.zsh"
+mkdir -p "$HOME/.zsh/functions"
 
 src="$dirname/zshrc"
 dst="$HOME/.zshrc"
 _link "$src" "$dst"
 
 src="$dirname/c"
-dst="$HOME/.zsh/c"
+dst="$HOME/.zsh/functions/c"
 _link "$src" "$dst"
 
 src="$dirname/_c"
-dst="$HOME/.zsh/_c"
+dst="$HOME/.zsh/functions/_c"
 _link "$src" "$dst"
