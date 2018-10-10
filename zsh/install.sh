82f257af58a6eef32ee0f764888af3064abffd46
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-10-09 23:56:23 -0700

    init c
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- zsh/install.sh
+++ zsh/install.sh
@@ -3,7 +3,16 @@
 dirname="$(dirname "$(readlink -f "$0")")"
 source "$dirname/../scripts/_shared.sh"
 
+mkdir -p "$HOME/.zsh"
+
 src="$dirname/zshrc"
 dst="$HOME/.zshrc"
+_link "$src" "$dst"
 
+src="$dirname/c"
+dst="$HOME/.zsh/c"
+_link "$src" "$dst"
+
+src="$dirname/_c"
+dst="$HOME/.zsh/_c"
 _link "$src" "$dst"
